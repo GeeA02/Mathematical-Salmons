18d6193ca4be911738a61e253aac0e20a2d222fc
Dominik Tyc <domikxf@gmail.com>
2020-11-15 20:38:31 +0100

    Add simple movement with animation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offsetPosition = new Vector3(0.0f, 4.0f, -5.0f);
+
+    private void Update()
+    {
+        if (target is null)
+        {
+            Debug.LogWarning("Missing target ref!", this);
+            return;
+        }
+        
+        transform.position = target.position + offsetPosition;
+    }
+}
---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HeroController : MonoBehaviour
+{
+    private readonly float movSpeed = 2;
+    private readonly float rotSpeed = 4;
+    private Vector3 oldPos = Vector3.zero;
+    private readonly Dictionary<KeyCode, bool> isDirectionKeyPressed = new Dictionary<KeyCode, bool>();
+    private CharacterController controller;
+    private Animator anim;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+        anim.SetBool("isIdle", true);
+        anim.SetBool("isWalking", false);
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        var newPos = new Vector3(horizontal, 0f, vertical).normalized;
+
+        controller.Move(newPos * movSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.W)) WalkAnimation(KeyCode.W, true);
+        if (Input.GetKeyDown(KeyCode.S)) WalkAnimation(KeyCode.S, true);
+        if (Input.GetKeyDown(KeyCode.A)) WalkAnimation(KeyCode.A, true);
+        if (Input.GetKeyDown(KeyCode.D)) WalkAnimation(KeyCode.D, true);
+        if (Input.GetKeyUp(KeyCode.W)) WalkAnimation(KeyCode.W, false);
+        if (Input.GetKeyUp(KeyCode.S)) WalkAnimation(KeyCode.S, false);
+        if (Input.GetKeyUp(KeyCode.A)) WalkAnimation(KeyCode.A, false);
+        if (Input.GetKeyUp(KeyCode.D)) WalkAnimation(KeyCode.D, false);
+
+        // while idle look at old "position"
+        if (newPos == Vector3.zero)
+            newPos = oldPos;
+        else
+            oldPos = newPos;
+
+        Quaternion rotation = Quaternion.LookRotation(newPos);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+                                            rotation,
+                                            rotSpeed * Time.deltaTime);
+    }
+
+    private void WalkAnimation(KeyCode key, bool isGonnaWalk)
+    {
+        if (isGonnaWalk)
+        {
+            // when any direction ket is not pressed
+            if (!isDirectionKeyPressed.Any(x => x.Value))
+            {
+                // enable walking animation
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isWalking", true);
+            }
+
+            isDirectionKeyPressed[key] = true;
+        }
+        else
+        {
+            isDirectionKeyPressed[key] = false;
+
+            if (!isDirectionKeyPressed.Any(x => x.Value))
+            {
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isWalking", false);
+            }
+        }
+    }
+}
