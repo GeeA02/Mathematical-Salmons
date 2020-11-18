03dfa2b517cd80fb04c6247d0ba66a3e009fb973
Dominik Tyc <domikxf@gmail.com>
2020-11-18 19:58:16 +0100

    Add CameraController and PlayerController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    public Transform target;
+    [SerializeField]
+    private Vector3 offsetPosition = new Vector3(0.0f, 5.0f, -7.0f);
+
+    private void Update()
+    {
+        if (target is null)
+        {
+            Debug.LogWarning("Missing target ref!", this);
+            return;
+        }
+        transform.position = target.position + offsetPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 5.0f;
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+    private Rigidbody playerBody;
+    private Vector3 inputVector;
+    new private Collider collider;
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal") * moveSpeed;
+        var vertical = Input.GetAxis("Vertical") * moveSpeed;
+        inputVector = new Vector3(horizontal, playerBody.velocity.y, vertical);
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+        playerBody.velocity = inputVector;
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    => Physics.Raycast(transform.position, Vector3.down, collider.bounds.size.y);
+}
