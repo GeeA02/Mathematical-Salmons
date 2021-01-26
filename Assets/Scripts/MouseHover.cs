f98f7aae927d6ac4ba4065c05695e6ad227704c2
Nynu <adrianszarnowski8@gmail.com>
2021-01-26 21:38:28 +0100

    Added a menu

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
