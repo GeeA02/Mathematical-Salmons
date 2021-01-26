8c42982498056a629b6d8939bedacec3b9d57932
Dominik Tyc <domikxf@gmail.com>
2021-01-26 13:40:15 +0100

    Add impact for Stranger's projectile

--- Assets/Scripts/StrangerProjectile.cs
+++ Assets/Scripts/StrangerProjectile.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class StrangerProjectile : MonoBehaviour
 {
     private bool isCollided;
+    public GameObject impactVFX;
     void OnCollisionEnter(Collision co)
     {
         if (co.gameObject.tag == "Player")
         {
             isCollided = true;
             co.gameObject.SendMessage("Damage");
+            var impact = Instantiate(impactVFX, co.contacts[0].point, Quaternion.identity) as GameObject;
+            Destroy(impact, 2);
             Destroy(gameObject);
         }
 
         else if (co.gameObject.tag != "Stranger_Projectile" && co.gameObject.tag != "Stranger" && !isCollided)
         {
-            isCollided = true;
+            isCollided = true; 
+            var impact = Instantiate(impactVFX, co.contacts[0].point, Quaternion.identity) as GameObject;
+            Destroy(impact, 2);
             Destroy(gameObject);
         }
     }
