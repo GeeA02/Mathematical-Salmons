16d228094afafe53326efa99dcfc9e64dbba5bac
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2021-01-27 13:21:50 +0100

    soft changes in menu

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    [SerializeField]
+    private float rotateForce = 70.0f;
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime, Space.Self);
-        transform.Rotate(new Vector3(0, 70, 0) * Time.deltaTime, Space.World);
+        transform.Rotate(new Vector3(0, rotateForce, 0) * Time.deltaTime, Space.World);
     }
 }
