7675187d339266a816bc2017b76017b211f20fcb
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2021-01-27 13:47:56 +0100

    Added menu sounds

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,9 +7,11 @@ public class MainMenu : MonoBehaviour
 {
     public bool isStart;
     public bool isQuit;
+    public AudioSource Click; 
 
     private void OnMouseUp()
     {
+        Click.Play();
         if(isStart)
         {
             SceneManager.LoadScene("map1");
