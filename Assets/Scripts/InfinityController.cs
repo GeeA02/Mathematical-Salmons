1913788917885b3ee31e54f3ab47aab15142646b
Dominik Tyc <domikxf@gmail.com>
2021-01-27 20:30:53 +0100

    Trampoline

---
+++ Assets/Scripts/InfinityController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfinityController : MonoBehaviour
+{
+    private Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        anim.SetTrigger("isBouncing");
+    }
+}
