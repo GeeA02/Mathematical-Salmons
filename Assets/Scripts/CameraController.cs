49c419687ee0e3531c698277077a369ae9b44e44
Dominik Tyc <domikxf@gmail.com>
2021-02-17 20:36:06 +0100

    change position of camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,33 +6,19 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
-
-    [SerializeField]
-    private Vector3 offsetPosition = new Vector3(-2.85f, 0.75f, -5.0f);
-
-
-    [SerializeField]
-    private Space offsetPositionSpace = Space.World;
-
-    [SerializeField]
-    private bool lookAt = true;
-
-
+    
     // Update is called once per frame
     private void Update()
     {
         if (target == null)
         {
-            //Debug.LogWarning("Missing target ref!", this);
+            Debug.LogWarning("Missing target ref!", this);
             return;
         }
-        // compute position 
-        if (offsetPositionSpace == Space.Self)
-            transform.position = target.TransformPoint(offsetPosition);
-        else
-            transform.position = target.position + offsetPosition;
-        // compute rotation
-        if (lookAt)
-            transform.LookAt(target);
+
+        transform.position = target.TransformPoint(0f, 1.5f, -2f);
+
+        transform.LookAt(target);
+        transform.rotation *= Quaternion.Euler(-25f, 0f, 0f);
     }
 }
